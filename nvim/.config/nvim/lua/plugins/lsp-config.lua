d8f6d810249df3b61c756848df60a04a95aa5a70
Jason Luck <jason.luck@gmail.com>
2025-11-08 21:20:22 -0600

    fixing nvim config errors

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,35 +1,38 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"terraformls",
-					"jdtls",
-					"gopls",
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
-			lspconfig.terraformls.setup({})
-			lspconfig.gopls.setup({})
-		end,
-	},
-	-- Plugin for Java LSP
-	{
-		"mfussenegger/nvim-jdtls",
-		config = function() end,
-	},
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "terraformls",
+          "jdtls",
+          "gopls",
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      --local lspconfig = require("lspconfig")
+      -- lspconfig.lua_ls.setup({})
+      -- lspconfig.terraformls.setup({})
+      -- lspconfig.gopls.setup({})
+      vim.lsp.config("lua_ls", {})
+      vim.lsp.config("terraformls", {})
+      vim.lsp.config("gopls", {})
+    end,
+  },
+  -- Plugin for Java LSP
+  {
+    "mfussenegger/nvim-jdtls",
+    config = function() end,
+  },
 }
